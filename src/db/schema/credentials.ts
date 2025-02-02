a4f6a987f99f0201a9b9c6d33cbf1cebc3057bb1
decimozs <marlonadiguemartin548@gmail.com>
2025-02-03 07:18:20 +0800

    feat: added indexes on db schema

--- src/db/schema/credentials.ts
+++ src/db/schema/credentials.ts
@@ -1,19 +1,27 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { index, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { nanoid } from "nanoid";
 
-export const trashbinCredentials = pgTable("trashbin_credentials", {
-  id: text("id")
-    .primaryKey()
-    .$defaultFn(() => nanoid())
-    .notNull(),
-  username: text("username").notNull(),
-  password: text("password").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at")
-    .notNull()
-    .$onUpdate(() => new Date()),
-});
+export const trashbinCredentials = pgTable(
+  "trashbin_credentials",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => nanoid())
+      .notNull(),
+    username: text("username").notNull(),
+    password: text("password").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at")
+      .notNull()
+      .$onUpdate(() => new Date()),
+  },
+  (table) => {
+    return {
+      usernameIdx: index("username_idx").on(table.username),
+    };
+  },
+);
 
 export const selectTrashbinCredentials =
   createSelectSchema(trashbinCredentials);
--- src/db/schema/trashbin.ts
+++ src/db/schema/trashbin.ts
@@ -1,25 +1,33 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { index, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { trashbinCredentials } from "./credentials";
 import { nanoid } from "nanoid";
 
-export const trashbinsTable = pgTable("trashbins", {
-  id: text("id")
-    .primaryKey()
-    .$defaultFn(() => nanoid())
-    .notNull(),
-  name: text("name").notNull(),
-  organization: text("organization").notNull(),
-  credentialId: text("credential_id")
-    .references(() => trashbinCredentials.id, {
-      onDelete: "cascade",
-    })
-    .notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at")
-    .notNull()
-    .$onUpdate(() => new Date()),
-});
+export const trashbinsTable = pgTable(
+  "trashbins",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => nanoid())
+      .notNull(),
+    name: text("name").notNull(),
+    organization: text("organization").notNull(),
+    credentialId: text("credential_id")
+      .references(() => trashbinCredentials.id, {
+        onDelete: "cascade",
+      })
+      .notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at")
+      .notNull()
+      .$onUpdate(() => new Date()),
+  },
+  (table) => {
+    return {
+      trashbinNameIdx: index("trashbin_name_idx").on(table.name),
+    };
+  },
+);
 
 export const selectTrashbin = createSelectSchema(trashbinsTable);
 
